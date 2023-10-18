8dc26a7378f3acb896aebdd9b2bdb006799adc82
ZYY\fauzi <fauzifirdaus7379@gmail.com>
2023-10-18 09:19:25 +0700

    Merubah Class Order

--- srp/Order.java
+++ srp/Order.java
@@ -13,13 +13,30 @@ class Order {
     }
     void deleteItem(Item item) {
     /*...*/
+    } 
     }
-    void printOrder() {
-    /*...*/
-    }
-    void showOrder() {
-    /*...*/
-    }
-    void getDailyHistory() {}
-    void getMonthlyHistory() {}
-    }
+    class OrderHistory {
+        void getDailyHistory() {}
+        
+        void getMonthlyHistory() {}
+        }
+
+        class OrderViewer {
+            void printOrder(Order order) {
+            }
+
+            void showOrder(Order order) {
+            }
+            }
+
+            class Main {
+                public static void main(String[] args) {
+                Item item = new Item();
+                Order order = new Order();
+                order.addItem(item);
+                OrderHistory history = new OrderHistory();
+                history.getDailyHistory();
+                OrderViewer viewer = new OrderViewer();
+                viewer.printOrder(order);
+                }
+                }
